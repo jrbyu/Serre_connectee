6d6c1432a0844dd71755f896748d162e0d9df252
Kan <123749997+jrbyu@users.noreply.github.com>
2025-04-03 11:09:45 +0200

    Update lum.ino

--- lum.ino
+++ lum.ino
@@ -1,24 +1,14 @@
-/*
- * Created by ArduinoGetStarted.com
- *
- * This example code is in the public domain
- *
- * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-light-sensor
- */
 
 void setup() {
-  // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
 }
 
 void loop() {
-  // reads the input on analog pin A0 (value between 0 and 1023)
   int analogValue = analogRead(A0);
 
   Serial.print("Analog reading: ");
-  Serial.print(analogValue);   // the raw analog reading
+  Serial.print(analogValue);  
 
-  // We'll have a few threshholds, qualitatively determined
   if (analogValue < 10) {
     Serial.println(" - Dark");
   } else if (analogValue < 200) {
